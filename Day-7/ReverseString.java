84de18df3b7316e430bb4e9b0c1b5b0b03fc07d4
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-05-05 07:15:32 +0530

    Implement reverseString method in Solution class

---
+++ Day-7/ReverseString.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static String reverseString(String s) {
+        
+        String result = "";
+        
+       
+        for(int i = s.length() - 1; i >= 0; i--) {
+            result = result + s.charAt(i);
+        }
+        
+        return result;
+    }
+}
