e195a9d21ff5340c0fc42aea0774a514ed89a89b
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-05-06 21:35:53 +0530

    Add method to remove spaces from a string

---
+++ Day-8/Day-8/RemoveSpaces.java
@@ -0,0 +1,15 @@
+class Solution {
+    String removeSpaces(String s) {
+        
+        String result = "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            if(s.charAt(i) != ' ') {
+                result = result + s.charAt(i);
+            }
+        }
+        
+        return result;
+    }
+}
