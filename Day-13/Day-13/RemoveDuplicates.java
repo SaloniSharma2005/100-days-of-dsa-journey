5f6725455887c32ed943ce516c198579e0a7a165
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-05-11 21:20:15 +0530

    Implement removeDuplicates method in Solution class

---
+++ Day-13/Day-13/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        if (arr.length == 0) return res;
+        
+        res.add(arr[0]); 
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                res.add(arr[i]);
+            }
+        }
+        
+        return res;
+    }
+}
