0a84446e51953751250f6d8d2bf8810c16ca577e
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-05-14 21:07:19 +0530

    Implement maximum product subarray solution

---
+++ Day-16/Day-16/MaximumProductSubarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    int maxProduct(int[] arr) {
+        
+        int maxProd = arr[0];
+        int minProd = arr[0];
+        int ans = arr[0];
+        
+        for(int i = 1; i < arr.length; i++) {
+            
+            if(arr[i] < 0) {
+                
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+          
+            maxProd = Math.max(arr[i], maxProd * arr[i]);
+           
+            minProd = Math.min(arr[i], minProd * arr[i]);
+            
+            ans = Math.max(ans, maxProd);
+        }
+        
+        return ans;
+    }
+}
