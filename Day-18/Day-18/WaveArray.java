4782777954a2b0acbbb55f5614aa80f62545cbe7
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-05-16 22:39:06 +0530

    Add wave array sorting method
    Implemented a method to sort an array in wave form by swapping adjacent elements.

---
+++ Day-18/Day-18/WaveArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortInWave(int arr[]) 
+    {
+        // code here
+        
+      for(int i=0; i<arr.length; i+=2)
+        {
+            
+           if(i==arr.length-1)
+            break;
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1]= temp;
+        }
+    }
+}
