ecf3835a742919d905c39753b8629243bf6e0a98
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-06-08 21:20:35 +0530

    Implement solution for kids with greatest candies

---
+++ Day-41/KidsWithGreatestNumberOfCandies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int maxCandies = 0;
+
+        for (int candy : candies) {
+            maxCandies = Math.max(maxCandies, candy);
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+
+        for (int candy : candies) {
+            ans.add(candy + extraCandies >= maxCandies);
+        }
+
+        return ans;
+    }
+}
