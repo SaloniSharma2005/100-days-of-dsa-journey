962a1b89bb5650b473a4df015eda40175fb24197
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-06-14 20:30:37 +0530

    Implement method to reverse words in a string

---
+++ Day-47/ReverseWordsInStringIII.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reverseWords(String s) {
+
+        String[] words = s.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+
+            StringBuilder rev = new StringBuilder(word);
+            ans.append(rev.reverse()).append(" ");
+        }
+
+        return ans.toString().trim();
+    }
+}
