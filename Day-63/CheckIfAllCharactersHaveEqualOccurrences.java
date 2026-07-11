46c405e252259fc23420f5c3115207efe3fbb9dd
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-07-11 21:18:48 +0530

    Implement method to check character occurrence equality

---
+++ Day-63/CheckIfAllCharactersHaveEqualOccurrences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        int count = 0;
+
+        for (int f : freq) {
+
+            if (f == 0) continue;
+
+            if (count == 0) {
+                count = f;
+            }
+            else if (count != f) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
