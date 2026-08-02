8610797bc05cfb534861256a4ec02b39d4da2f7c
saloni sharma <145436884+SaloniSharma2005@users.noreply.github.com>
2026-08-02 20:59:19 +0530

    Implement numComponents method for linked list

---
+++ Day-85/LinkedListComponents.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numComponents(ListNode head, int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int count = 0;
+
+        while (head != null) {
+
+            if (set.contains(head.val) &&
+               (head.next == null || !set.contains(head.next.val))) {
+
+                count++;
+            }
+
+            head = head.next;
+        }
+
+        return count;
+    }
+}
